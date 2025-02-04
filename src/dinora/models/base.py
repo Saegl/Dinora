e09b6bc42b57f4346a69d239338e11c8c5540310
Saegl <saegl@protonmail.com>
2025-02-04 14:08:18 +0500

    Add BaseModel doc comments

--- src/dinora/models/base.py
+++ src/dinora/models/base.py
@@ -14,11 +14,28 @@ class BaseModel(ABC):
 
     @abstractmethod
     def evaluate(self, board: chess.Board) -> Evaluation:
-        pass
+        """
+        Evaluate board by returning policy and value
+
+        sum(policy.values()) == 1.0
+        -1.0 <= value <= 1.0
+        value = 1.0 => Current side (board.turn) is winning
+        value = -1.0 => Current side (board.turn) is losing
+        value = 0.0 => Draw
+        """
 
     def evaluate_batch(self, boards: list[chess.Board]) -> list[Evaluation]:
+        """
+        Same as `evaluate` but for batch of boards
+
+        Defaults to slow fallback, use faster methods of your
+        neural networks library
+        """
         return [self.evaluate(board) for board in boards]
 
     @abstractmethod
     def reset(self) -> None:
-        """Delete caches"""
+        """
+        Delete caches
+        Useful to call between games in benchmarks/evaluators
+        """
